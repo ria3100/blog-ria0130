05792cc1dd724a0b3ba1cd1ad68606983a80fd56
ria <ria000130@gmail.com>
2020-06-11 17:37:58 +0900

    コードブロックにファイルアイコンを追加 けどダサかったからコメントアウト

--- components/atoms/NamedCodeBlock.tsx
+++ components/atoms/NamedCodeBlock.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react'
+// import { fetchFileIcon } from 'utils/fileIcons'
 
 type Props = { filename: string }
 export const NamedCodeBlock: React.FC<Props> = ({ filename, children }) => {
+  // const fileIcon = fetchFileIcon(filename)
+
   return (
     <div>
-      <div className="px-4">{filename}</div>
+      <div className="px-4">
+        {/* {fileIcon && (
+          <img
+            className="w-4 mr-1 mt-1 float-left"
+            src={`/material-icons/${fileIcon}.svg`}
+          />
+        )} */}
+        {filename}
+      </div>
       {children}
     </div>
   )

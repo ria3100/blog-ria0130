5a7efca303146ed2720dfff5edd4cbe390eef91c
Ria <ria000130@gmail.com>
2020-06-17 23:22:37 +0900

    スマホ実機でコードブロックが横スクロールできなかった

--- components/organisms/Title.tsx
+++ components/organisms/Title.tsx
@@ -5,7 +5,7 @@ type Props = { title: string; date: string }
 export const Title: React.FC<Props> = ({ title, date }) => {
   return (
     <div className="flex flex-col justify-center h-400px container mx-auto w-full">
-      <h1 className="font-product-sans font-bold text-4xl tracking-wide text-center text-gray-100">
+      <h1 className="font-product-sans font-bold text-4xl tracking-wide text-center text-gray-100 px-4">
         {title}
       </h1>
 

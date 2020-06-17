49e82811d965e2cfbcd6d08a14ff7671ee30bd0a
ria <ria000130@gmail.com>
2020-06-17 14:35:50 +0900

    recoilの記事

--- components/organisms/Title.tsx
+++ components/organisms/Title.tsx
@@ -4,7 +4,7 @@ type Props = { title: string; date: string }
 
 export const Title: React.FC<Props> = ({ title, date }) => {
   return (
-    <div className="flex flex-col justify-center h-400px w-full">
+    <div className="flex flex-col justify-center h-400px container mx-auto w-full">
       <h1 className="font-product-sans font-bold text-4xl tracking-wide text-center text-gray-100">
         {title}
       </h1>
